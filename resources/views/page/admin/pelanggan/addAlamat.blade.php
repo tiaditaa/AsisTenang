b3ddac38811f230aa812f6c77a45234264a6cc67
Aryagara Kristandy <72059961+kristandy@users.noreply.github.com>
2023-11-30 07:56:00 +0700

    fix addAlamat

--- resources/views/page/admin/pelanggan/addAlamat.blade.php
+++ resources/views/page/admin/pelanggan/addAlamat.blade.php
@@ -49,16 +49,15 @@
                     </div>
                     <div class="card-body">
                         <div class="form-group">
-                            <label for="inputNamaAlamat">Nama Alamat</label>
+                            <label for="nama_alamat">Nama Alamat</label>
                             <input
                                 type="text"
-                                id="inputNamaAlamat"
-                                name="name"
-                                class="form-control @error('name') is-invalid @enderror"
+                                id="nama_alamat"
+                                name="nama_alamat"
+                                class="form-control @error('nama_alamat') is-invalid @enderror"
                                 placeholder="Masukkan Nama Alamat"
-                                value="{{ old('name') }}"
                                 required="required"
-                                autocomplete="name">
+                                autocomplete="off">
                             @error('name')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -67,83 +66,44 @@
                         </div>
                         <div class="form-group">
                             <label for="inputProvinsi">Provinsi</label>
-                            <select class="form-control" name="" id="">
-                                <option value="">Provinsi</option>
-                                <option value="">Provinsi</option>
+                            <select class="form-control" name="id_provinsi" id="id_provinsi">
+                                <option value="" disabled selected>Pilih Provinsi</option>
+                                @foreach ($provinsiList as $provinsi)
+                                    <option value="{{ $provinsi->id }}">{{ $provinsi->nama_provinsi }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="inputKota">Kota</label>
-                            <select class="form-control" name="" id="">
-                                <option value="">Kota</option>
-                                <option value="">Kota</option>
+                            <select class="form-control" name="id_kota" id="id_kota">
+                                <option value="" disabled selected>Pilih Kota</option>
+                                @foreach ($kotaList as $kota)
+                                    <option value="{{ $kota->id }}">{{ $kota->nama_kota }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="inputKecamatan">Kecamatan</label>
-                            <select class="form-control" name="" id="">
-                                <option value="">Kecamatan</option>
-                                <option value="">Kecamatan</option>
+                            <select class="form-control" name="id_kecamatan" id="id_kecamatan">
+                                <option value="" disabled selected>Pilih Kecamatan</option>
+                                @foreach ($kecList as $kecamatan)
+                                    <option value="{{ $kecamatan->id }}">{{ $kecamatan->nama_kec }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="form-group">
-                            <label for="inputKodePos">Kode Pos</label>
-                            <input type="text" class="form-control" id="inputKodePos" placeholder="Masukkan kode pos">
+                            <label for="kode_pos">Kode Pos</label>
+                            <input type="text" class="form-control" id="kode_pos" placeholder="Masukkan kode pos">
                         </div>
                         <div class="form-group">
-                            <label for="inputAlamat">Alamat Lengkap</label>
-                            <textarea name="inputAlamat" id="inputAlamat" class="form-control" cols="30" rows="5" required></textarea>
+                            <label for="alamat_lengkap">Alamat Lengkap</label>
+                            <textarea name="alamat_lengkap" id="alamat_lengkap" class="form-control" cols="30" rows="5" required></textarea>
                         </div>
                     </div>
                     <!-- /.card-body -->
                 </div>
                 <!-- /.card -->
             </div>
-            <div class="col-md-6">
-                <div class="card card-secondary">
-                    <div class="card-header">
-                        <h3 class="card-title">Password</h3>
-
-                        <div class="card-tools">
-                            <button
-                                type="button"
-                                class="btn btn-tool"
-                                data-card-widget="collapse"
-                                title="Collapse">
-                                <i class="fas fa-minus"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input
-                                id="password"
-                                type="password"
-                                placeholder="Kata Sandi"
-                                class="form-control @error('password') is-invalid @enderror"
-                                name="password"
-                                required="required"
-                                autocomplete="new-password">
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password-confirm">Konfirmasi Password</label>
-                            <input
-                                placeholder="Ketik ulang kata sandi"
-                                id="password-confirm"
-                                type="password"
-                                class="form-control"
-                                name="password_confirmation"
-                                required="required"
-                                autocomplete="new-password">
-                        </div>
-                    </div>
-                    <!-- /.card-body -->
                 </div>
                 <!-- /.card -->
             </div>
